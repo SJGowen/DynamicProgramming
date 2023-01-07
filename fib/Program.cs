f0b2eee62fe9b3bc93b48008c127e9384270decc
Stephen Gowen <sjgowen@gmail.com>
2023-01-07 23:26:15 +0000

    Added a SafeFibonacci method which calls Fibonacci in a try block. Changed the return type from string to BigInteger as this can be more easily used.

--- fib/Program.cs
+++ fib/Program.cs
@@ -6,29 +6,43 @@ public class Program
 {
     static void Main()
     {
-        Console.WriteLine(Fibonacci(-1));
-        Console.WriteLine(Fibonacci(0));
-        Console.WriteLine(Fibonacci(6));
-        Console.WriteLine(Fibonacci(7));
-        Console.WriteLine(Fibonacci(8));
-        Console.WriteLine(Fibonacci(50));
-        Console.WriteLine(Fibonacci(250));
-        Console.WriteLine(Fibonacci(500));
+        SafeFibonacci(-1);
+        SafeFibonacci(0);
+        SafeFibonacci(7);
+        SafeFibonacci(8);
+        SafeFibonacci(50);
+        SafeFibonacci(250);
+        SafeFibonacci(500);
     }
 
-    public static string Fibonacci(int n)
+    public static void SafeFibonacci(int n)
     {
-        if (n < 0) { return $"The handling of negative numbers is not handled by this implementation of Fibonacci! (argument passed = {n})"; }
-        BigInteger[] memoizator = new BigInteger[n + 1];
-        return Fibonacci(n, memoizator).ToString();
+        try
+        {
+            Console.WriteLine(Fibonacci(n));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 
-    private static BigInteger Fibonacci(int n, BigInteger[] memoizator)
+    public static BigInteger Fibonacci(int n)
     {
-        if (memoizator[n] != 0) return memoizator[n];
+        if (n < 0) 
+        { 
+            throw new ArgumentException($"The handling of negative numbers ({n}) is not handled by this implementation of Fibonacci!"); 
+        }
+        var memo = new BigInteger[n];
+        return Fibonacci(n, memo);
+    }
+
+    private static BigInteger Fibonacci(int n, BigInteger[] memo)
+    {
+        if (memo[n] != 0) return memo[n];
         if (n <= 2) return BigInteger.One;
-        memoizator[n] = Fibonacci(n - 1, memoizator) + Fibonacci(n - 2, memoizator);
-        return memoizator[n];
+        memo[n] = Fibonacci(n - 1, memo) + Fibonacci(n - 2, memo);
+        return memo[n];
     }
 }
 
